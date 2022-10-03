3d1a84553fd831886037242e54a25bc313d13661
Kaylee Carroll <kayleec678@gmail.com>
2022-10-03 15:31:36 -0400

    initial commit

--- Lab/Lab Reports /Coding Diagram Script.R
+++ Lab/Lab Reports /Coding Diagram Script.R
@@ -1,5 +1,4 @@
-file_name <- readline()
-MachineRead_output <- read_csv(file_name)
+MachineRead_output <- read.csv("MachineRead_output.csv")
 MachineRead_output$Paper.ID <- gsub("\\[","",MachineRead_output$Paper.ID)
 MachineRead_output$Paper.ID <- gsub("\\]","",MachineRead_output$Paper.ID)
 MachineRead_output$Paper.ID <- gsub("\\'","",MachineRead_output$Paper.ID)
@@ -8,13 +7,13 @@ MachineRead_output$Paper.ID <- gsub(" ","",MachineRead_output$Paper.ID)
 
 paper_ids <-c()
 for(each in MachineRead_output$Paper.ID){
-  paper_ids <- unlist(strsplit(each,","))
+paper_ids <- unlist(strsplit(each,","))
+
+row_num <- which(each == MachineRead_output$Paper.ID)
+
+MachineRead_output$number_unique[row_num] <- length(unique(paper_ids))
   
-  row_num <- which(each == MachineRead_output$Paper.ID)
-  
-  MachineRead_output$number_unique[row_num] <- length(unique(paper_ids))
-  
-}
+  }
 row_to_remove <- which(MachineRead_output$number_unique == 1)
 MachineRead_output <-MachineRead_output[-c(row_to_remove),]
 
---
+++ Lab/Lab Reports /User Input Script.R
@@ -0,0 +1,21 @@
+file_name <- readline()
+MachineRead_output <- read_csv(file_name)
+MachineRead_output$Paper.ID <- gsub("\\[","",MachineRead_output$Paper.ID)
+MachineRead_output$Paper.ID <- gsub("\\]","",MachineRead_output$Paper.ID)
+MachineRead_output$Paper.ID <- gsub("\\'","",MachineRead_output$Paper.ID)
+MachineRead_output$Paper.ID <- gsub(" ","",MachineRead_output$Paper.ID)
+
+
+paper_ids <-c()
+for(each in MachineRead_output$Paper.ID){
+  paper_ids <- unlist(strsplit(each,","))
+  
+  row_num <- which(each == MachineRead_output$Paper.ID)
+  
+  MachineRead_output$number_unique[row_num] <- length(unique(paper_ids))
+  
+}
+row_to_remove <- which(MachineRead_output$number_unique == 1)
+MachineRead_output <-MachineRead_output[-c(row_to_remove),]
+ 
+
